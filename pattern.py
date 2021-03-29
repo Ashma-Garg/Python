f247cf70e619c5bf51c4ae923d71a8a502e65727
Ashma-Garg <ashmagarg77u@gmail.com>
2021-03-29 22:54:18 +0530

    Short tricks

---
+++ pattern.py
@@ -0,0 +1,15 @@
+# You are given a positive integer . Print a numerical triangle of height  like the one below:
+#
+# 1
+# 22
+# 333
+# 4444
+# 55555
+# ......
+
+
+
+
+
+for i in range(1,input()):
+    print (((10**i)/9)*i)
